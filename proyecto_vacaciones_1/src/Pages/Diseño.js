34a24cd6142c3980ccf537768b84ea16a888b136
tartaman <70653340+tartaman@users.noreply.github.com>
2025-04-23 22:24:02 -0600

    nais

--- proyecto_vacaciones_1/src/Pages/Diseño.js
+++ proyecto_vacaciones_1/src/Pages/Diseño.js
@@ -8,8 +8,36 @@ export default function Diseño() {
 
         </div>
         <div className='Volver'>
-
+          <div className='inside'>
+            <h3>&lt; Volver</h3>
+          </div>
+          <h1>
+            RICK Y MORTY AQUI PARA <br/> QUEDARSE SIEMPRE
+          </h1>
         </div>
+      </div>
+      <div className='Content'>
+        <p className='textInDesign'>
+        Para brindar un entorno agradable y seguro a todos nuestros huéspedes estamos trabajando para alinearnos con las nuevas regulaciones de control de uso de tabaco. Por lo anterior, estamos haciendo algunas modificaciones en las áreas para fumadores dentro de nuestros resorts, creando espacios específicos que se ajusten a los requerimientos establecidos por ley, que incluyen lo siguiente:
+        </p>
+        <img style={{width: '100%', height: '25rem'}} alt="imagen lanscape" src="https://ew.com/thmb/JlgVoveapHG90XSzHzMQNHPje44=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/rick-and-morty-season-4-2000-828c3128b0f74863a4153056e6051ab6.jpg"/>
+        <ul>
+          <li>
+          "Morty, escúchame bien... el multiverso no va a salvarse solo, Morty. Tenemos que encontrar el cristal cuántico de Zorpazorp antes de que los Cronenbergs se fusionen con los Gromflomitas y hagan estallar la línea de tiempo, Morty. No, no podemos detenernos por una soda ni por un baño. ¡La entropía no espera! ¿Ves esa anomalía cuántica en el cielo? Es el resultado directo de tus malas decisiones en la dimensión F-672. Así que súbete a la nave, ajusta el conversor de neutrinos y prepárate para experimentar cosas que ni siquiera el Consejo de Ricks aprobaría, Morty."
+          </li>
+          <li>
+          "La vida es solo una simulación de código roto sobre un servidor etéreo abandonado por dioses programadores, Morty. ¿Y sabes qué hacemos los inteligentes? ¡Nos tomamos un trago y lo pirateamos para jugar a ser los jefes del universo! [eructo] Ahora pásame la pistola de portales, tengo que ir a una dimensión donde soy un pepinillo pero también el presidente."
+          </li>
+          <li>
+          "¿En serio me trajeron a una dimensión donde la moda son sombreros parlantes que insultan a sus dueños? Estoy tan cansada de que mis traumas adolescentes se usen como combustible para aventuras galácticas. Ni siquiera hay WiFi decente aquí."
+          </li>
+        </ul>
+        <img style={{width: '100%', height: '25rem'}} alt="imagen lanscape" src="https://abbiesartblog.wordpress.com/wp-content/uploads/2019/01/s2e3_mount_morty_and_summer.png"/>
+        <p className='textInDesign'>
+        "Beth, creo que si le hablo a la serpiente del tiempo con firmeza, tal vez... tal vez entienda que no queremos problemas. Después de todo, leí en internet que los reptiles responden bien al tono pacífico. ¿Dónde están mis chanclas?"
+        </p>
+        <img style={{ marginBottom:'15rem',width: '100%', height: '25rem'}} alt="imagen land" src="https://image.isu.pub/210820205412-44ed29d9c32fe2fc671d2f4f479c2128/jpg/page_1_thumb_large.jpg"/>
+        <hr style={{marginBottom:'1rem', width:'100%'}}/>
       </div>
     </div>
   )
